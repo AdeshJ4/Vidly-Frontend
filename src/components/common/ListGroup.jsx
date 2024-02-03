b357ca9548861db65b7807bf17ce8508747eb354
AdeshJ4 <adejad1234@gmail.com>
2024-02-03 09:32:46 +0530

    Implement 'All Genres' list button in ListGroup COmponent

--- src/components/common/ListGroup.jsx
+++ src/components/common/ListGroup.jsx
@@ -4,14 +4,24 @@ const ListGroup = ({ genres, selectedGenre, onGenreSelect }) => {
   console.log(genres);
   return (
     <ul className="list-group">
+      <li
+        onClick={() => onGenreSelect(null)}
+        className={selectedGenre ? "list-group-item" : "list-group-item active"}
+      >
+        All Genres
+      </li>
       {genres.map((genre) => (
         <li
           key={genre._id}
           onClick={() => onGenreSelect(genre)}
           className={
-            genre === selectedGenre ? "list-group-item active" : "list-group-item"
+            genre === selectedGenre
+              ? "list-group-item active"
+              : "list-group-item"
           }
-        >{genre.name}</li>
+        >
+          {genre.name}
+        </li>
       ))}
     </ul>
   );
